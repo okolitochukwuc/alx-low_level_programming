40977c3767fa7a34b5170588ae9169b453151523
Okoli Tochukwu <okolitochukwuc@gmail.com>
2023-06-19 07:35:34 -0700

    tran

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main : a C program that prints line using puts
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
